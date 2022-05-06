70472fbd506ef7c674a1726d11d88d7d3d120254
mubshar aziz <mubasharaziz2020@gmail.com>
2022-05-06 21:21:00 +0400

    added user model

---
+++ models/Users.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const moment = require('moment');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  passwordagain: {
+    type: String,
+    required: true,
+  },
+  profile_pic: {
+    type: String,
+    default: 'empty_avatar_jpg',
+  },
+  createdAt: {
+    type: String,
+    default: moment().format('DD/MM/YYYY') + ';' + moment().format('hh:mm:ss'),
+  },
+  updatedAt: {
+    type: String,
+    default: moment().formate('DD/MM/YYYY') + ';' + moment().format('hh:mm:ss'),
+  },
+});
+
+//    created user model
+
+mongoose.model('users', userSchema);
+
+//    export user model
+
+module.exports = mongoose.model('users');
